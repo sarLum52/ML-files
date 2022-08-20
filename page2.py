02503b3948335f29846eea73f21789cb78cff3e2
Sarah Lum <47491959+sarLum52@users.noreply.github.com>
2022-08-19 21:54:37 -0700

    Add files via upload

--- page2.py
+++ page2.py
@@ -5,16 +5,26 @@ import keras as keras
 import tkinter as tk
 from tkinter import filedialog
 from PIL import Image
+from tkinter import *
+
 
 newModel = keras.models.load_model("my_model")
 
 newModel.compile(loss='mse',optimizer='Adam')
-path = filedialog.askopenfilename(filetypes = [("JPEG files",  "*.jpg")])
-pic = Image.open(path)
-print(path)
+
+frame = Tk()
+frame.geometry("700x350")
+
+def uploadFile():
+
+    global path
+    path = filedialog.askopenfilename(filetypes = (("JPEG files","*.jpeg"), ("JPEG files", "*.jpg")))
+    #pic = Image.open(path)
+    #print(path)
 
 def onSubmit():
-
+    print("dsfsdf")
+    print(path)
     img = tf.keras.utils.load_img(
         path, target_size=(160, 160)
     )
@@ -30,4 +40,10 @@ def onSubmit():
         .format(class_names[np.argmax(score)], 100 * np.max(score))
     )
 
-onSubmit()
+#path = "C:/Users/Me/Pictures/Saved Pictures/healthybrain.jpeg"
+
+tk.Button(frame, text="Upload file", command = uploadFile, font= ('Aerial 17 bold italic')).pack(pady= 30)
+
+tk.Button(frame, text="submit", command = onSubmit).pack(pady= 20)
+
+frame.mainloop()
